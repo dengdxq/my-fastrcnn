162d97a7cdd760d2fb13b28b5ef3ebdd1378e50d
王加佳 <wangjj@oukakas-MacBook-Pro.local>
2016-06-03 15:51:18 +0800

    _init_config.py

---
+++ pyscript/_init_config.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 –*-
+
+import os.path as osp
+import sys
+
+prototxt = '/home/jiayuan/Documents/download_code/fast-rcnn/models/checkcode/test.prototxt'
+caffemodel = '/home/jiayuan/Documents/download_code/fast-rcnn/data/fast_rcnn_models/checkcode_fast_rcnn_iter_200000.caffemodel'
+img_save_dir = '/home/jiayuan/Documents/fastrcnn/images'
+log_file = '/home/jiayuan/Documents/fastrcnn/log'
