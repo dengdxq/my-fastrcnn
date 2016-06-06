84b83b26775bb9740c1052c2b30c780699aac583
王加佳 <wangjj@oukakas-MacBook-Pro.local>
2016-06-06 17:49:12 +0800

    modify _init_config.py;rm _init_configure.py

--- pyscript/_init_config.py
+++ pyscript/_init_config.py
@@ -5,5 +5,15 @@ import sys
 
 prototxt = '/home/jiayuan/Documents/download_code/fast-rcnn/models/checkcode/test.prototxt'
 caffemodel = '/home/jiayuan/Documents/download_code/fast-rcnn/data/fast_rcnn_models/checkcode_fast_rcnn_iter_200000.caffemodel'
+############################
+#IMAGE
+############################
 img_save_dir = '/home/jiayuan/Documents/fastrcnn/images'
+############################
+#LOG
+############################
 log_file = '/home/jiayuan/Documents/fastrcnn/log'
+log_file_size = 104857600 #100M=1024*1024*100
+log_file_name = 'checkcode.log'
+log_file_max_num = 50
+
