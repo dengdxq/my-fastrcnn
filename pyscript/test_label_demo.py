08203b2945f135346cbdc32efe35d40e7c8f79a6
jiayuan <jiayuan@wacai.com>
2016-06-14 11:38:14 +0800

    modify:test_label_demo.py

--- pyscript/test_label_demo.py
+++ pyscript/test_label_demo.py
@@ -4,22 +4,26 @@ import os
 import shutil
 import random
 
-IMAGE_DIR = ''
+
+PROTOTXT = '/data/code/my-fastrcnn/models/checkcode_vgg16/test.prototxt'
+CAFFEMODEL = '/data/code/my-fastrcnn/output/default/train/checkcode_vgg16_fast_rcnn_iter_190000.caffemodel'
 CLASS_TUPLE = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9')
 
 def get_file_list(path, suffix):
     filelist = []
     for root,dirs,files in os.walk(path):
-        strs = files.split('.')
-        if len(strs)!=2 or cmp(strs[1],suffix)!=0:
-            continue
-    filelist.extend(files)
+	for felem in files:
+		strs = felem.split('.')
+		if len(strs)!=2 or cmp(strs[1],suffix)!=0:
+		    continue
+	    	filelist.append(felem)
     return filelist
 
 
 def random_string(strlen):
     alpha_list = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a']
     name = random.sample(alpha_list, strlen)
+    name = ''.join(name)
     return name
 
 
@@ -37,23 +41,25 @@ def label_images(src_img_path, dst_path):
     imglist = get_file_list(src_img_path, 'jpg')
     num = len(imglist)
     for i in range(0,num):
+        print 'process: %d'%(i+1)
         imgpath = src_img_path + '/' + imglist[i]
         if os.path.exists(imgpath)==False:
             continue
         cc_value = fastrcnn.recognize_checkcode_img(caffe_net, imgpath, CLASS_TUPLE)
-        dpath = dst_path + '/' + cc_value + '.jpg'
-        if os.path.exists(imgpath)==True:
+        dpath = dst_path + '/' + cc_value['ccvalue'] + '.jpg'
+        if os.path.exists(dpath)==True:
             dpath = dst_path + '/' + cc_value['ccvalue'] + '_' + random_string(6) + '.jpg'
-        shutil.copyfile(imgpath, dst_path)
+        shutil.copyfile(imgpath, dpath)
         #char rect
         rects = cc_value['rects']
         pfile.write(imglist[i]+' ')
-        pfile.write(cc_value['ccvalue'])
+        pfile.write(cc_value['ccvalue']+' ')
+        pfile.write(str(len(rects)))
         for elem in rects:
-            pfile.write(' '+str(elem.get('xmin'))+' '+str(elem.get('ymin'))+str(elem.get('xmax'))+str(elem.get('ymax')))
+            pfile.write(' '+str(elem.get('xmin'))+' '+str(elem.get('ymin'))+' '+str(elem.get('xmax'))+' '+str(elem.get('ymax')))
         pfile.write('\n')
     pfile.close()
 
 
 if __name__=='__main__':
-    label_images('', '')
+    label_images('/data/Images/checkcode/data/TestImgs', '/data/Images/checkcode/data/TestImgs_label')
