a388a7cf5372885269ff26665566553ab6f216fa
jiayuan <jiayuan@wacai.com>
2016-08-18 17:39:09 +0800

    test

--- pyscript/tornado_http.py
+++ pyscript/tornado_http.py
@@ -2,6 +2,8 @@ import matplotlib
 matplotlib.use('Agg') 
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
+#import tornado.process
 import urlparse
 import fastrcnn
 import _init_config
@@ -84,14 +86,16 @@ class MainHandler(tornado.web.RequestHandler):
 		#---recognize---
 		start_time = time.time()
 		try:
-			print '==================================='
 			cc_value = fastrcnn.recognize_checkcode_img(CAFFE_NET, savepath, CLASS_TUPLE)['ccvalue']
+			#a = 1/0
 		except :
-			print '123232344'
-			#fp = StringIO.StringIO()    
-	    	#traceback.print_exc(file=fp)
-	    	#message = fp.getvalue()
-	    	#self.logger.info('[ERROR-start]: TID:%serrorinfo:\n%s\n[ERROR-end]'%(param_dict['tid'], message))
+			fp = StringIO.StringIO()
+			traceback.print_exc(file=fp)
+			message = fp.getvalue()
+			#print '[ERROR-start]: TID:%s'%(param_dict['tid'])
+			#print message
+			#print '[ERROR-end]'
+			self.logger.info('[ERROR-start]: TID:%s, errorinfo:\n%s\n[ERROR-end]'%(param_dict['tid'], message))
 		end_time = time.time()
 		cc_result = '-1'
 		if cc_value != '':
@@ -198,7 +202,13 @@ if __name__ == "__main__":
 	load_caffe_net()
 	logger.info('load caffe net complete!')
 	application = tornado.web.Application([(r"/checkcode", MainHandler),])
-	application.listen(8181)
-	logger.info('checkcode recognize server start!')
+	#sockets = tornado.netutil.bind_sockets(8181)
+	#tornado.process.fork_processes(4)
+	server = tornado.httpserver.HTTPServer(application)
+	server.bind(8181)
+	server.start(0)
 	tornado.ioloop.IOLoop.instance().start()
+	#application.listen(8181)
+	logger.info('checkcode recognize server start!')
+	#tornado.ioloop.IOLoop.instance().start()
 	logger.info('checkcode recognize server complete!')
