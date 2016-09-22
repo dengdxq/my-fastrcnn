d493a514d69bdf8732548c4380d4adc01a0aae4c
jiayuan <jiayuan@wacai.com>
2016-09-22 14:53:12 +0800

    增加了验证码是否识别正确

---
+++ pyscript/logconfig2.py
@@ -0,0 +1,53 @@
+#-*- coding:utf-8 –*-
+
+import logging.config
+import logging.handlers
+import logging
+import _init_config
+
+
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%Y-%m-%d %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file': {
+            'level': 'DEBUG',
+            #'class': 'logging.handlers.RotatingFileHandler',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            # 当达到10MB时分割日志
+            #'maxBytes': 1024 * 1024 * 10,
+            # 最多保留1000份文件
+            #'backupCount': 6000,
+            # If delay is true,
+            # then file opening is deferred until the first call to emit().
+            'delay': True,
+            'filename': '/data/online/logs/checkcode_iserror.log',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    }
+})
---
+++ pyscript/tornado_response.py
@@ -0,0 +1,46 @@
+#-*- coding:utf-8 –*-
+import sys 
+import tornado.ioloop
+import tornado.web
+import time
+import json
+import os
+import traceback
+import StringIO
+import logging.config
+import logging.handlers
+import logging
+import logconfig2
+
+
+class MainHandler(tornado.web.RequestHandler):	
+	logger = logging.getLogger('MainHandler')
+	def get(self):
+		param_dict = {}
+		param_dict['tid'] = self.get_argument('tid', '')
+		if len(param_dict['tid'])==0:
+			self.logger.info('The Params of tid is null!')
+			str_dict = {}
+			str_dict['errcode'] = 101
+			str_dict['errmsg'] = 'parameter error, loss tid!'
+			self.write(json.dumps(str_dict))
+			return
+		self.logger.info('GET Params:%s'%(json.dumps(param_dict)))
+		#
+		self.logger.info('ERROR_TASKID=%s'%(param_dict['tid']))
+		str_dict = {}
+		str_dict['errcode'] = 0
+		str_dict['errmsg'] = 'ok!'
+		self.write(json.dumps(str_dict))
+
+
+if __name__ == "__main__":
+	port = sys.argv[1]
+	thread_name = port
+	logger = logging.getLogger('main')
+	logger.info('restart isright server!')
+	application = tornado.web.Application([(r"/ccright", MainHandler),])
+	application.listen(int(port))
+	logger.info('checkcode isright server start!')
+	tornado.ioloop.IOLoop.instance().start()
+	logger.info('checkcode isright server start complete!')
